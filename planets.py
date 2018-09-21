83baf823cf902af452c4a7b239e0b97980819c30
AlexMaclean <aemaclea@calpoly.edu>
2018-09-21 10:41:16 -0700

    I think final commit

--- planets.py
+++ planets.py
@@ -1,7 +1,9 @@
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    weight = int(input("What do you weigh on earth? "))
+    print()
+    print("On Mars you would weigh " + str(weight * 0.38) + " pounds.")
+    print("On Jupiter you would weigh " + str(weight * 2.34) + " pounds.")
+
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
